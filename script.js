c00ac5527a287ae20e7a93d1e92b91b2502447b6
illiatishchenko98 <102582375+illiatishchenko98@users.noreply.github.com>
2023-02-27 21:19:01 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// // let number = 5;
+
+
+// // function logNumber() {
+// //     console.log(number);
+// // }
+
+// // number = 6;
+
+
+// // logNumber();
+
+
+// function creatCounter() {
+//     let counter = 0;
+
+//     const myFunction = function() {
+//         counter = counter + 1;
+//         return counter;
+//     }
+//     return myFunction;
+// };
+
+
+// const increment = creatCounter();
+
+// const c1 = creatCounter();
+// const c2 = creatCounter();
+// const c3 = creatCounter();
+
+
+// console.log(c1, c2, c3);
+
+
+
+// let x = 5;
+// alert( x++ );
+// alert( ++x );
+
+
+// let y = 1;
+// let x = y = 2;
+// console.log(x);
+
+
+// console.log([] + 1 +2);
+
+// alert("1"[0]);
+
+// console.log(2 && 1 && null && 0 && undefined);
+
+// console.log(!!( a && b ) === (a && b));
+
+// alert(null || 2 && 3 && 4);
+
+// const a = [1, 2, 3];
+// const b = [1, 2, 3];
+// console.log(a == b);
+
+// alert(+'Infinity');
+
+
